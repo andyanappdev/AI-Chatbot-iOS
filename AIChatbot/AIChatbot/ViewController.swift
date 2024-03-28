4e49ce9ed3a1d7ec3add0d5565d76999140e1769
Andy An <andyanappdev@gmail.com>
2024-03-28 11:43:21 +0900

    config(*): Setup iOS project
    - Delete storyboard

---
+++ AIChatbot/AIChatbot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AIChatbot
+//
+//  Created by Doyoung An on 3/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
