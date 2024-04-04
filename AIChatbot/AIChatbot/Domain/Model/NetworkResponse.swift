b3089ca9cfcb49e272237db9921e3b95bf047095
Andy An <andyanappdev@gmail.com>
2024-04-04 16:08:31 +0900

    feat(NetworkResponse): Define NetworkResponse data model

---
+++ AIChatbot/AIChatbot/Domain/Model/NetworkResponse.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkResponse.swift
+//  AIChatbot
+//
+//  Created by Doyoung An on 4/4/24.
+//
+
+import Foundation
+
+struct NetworkResponse {
+    let response: HTTPURLResponse
+    let data: Data?
+}
